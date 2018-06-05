cf11b65b75e103774d49f120320715fcc3a2f1c7
9984199774 <984199774@qq.com>
2018-06-06 07:34:10 +0800

    初始化提交代码

---
+++ FMTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxs
+{
+    public partial class FMTest : Form
+    {
+        public void ShowMessage(string msg)
+        {
+            this.Dispatcher.Invoke(new MessageBoxShow(MessageBoxShow_F), new object[] { msg });
+        }
+
+        delegate void MessageBoxShow(string msg);
+        void MessageBoxShow_F(string msg)
+        {
+            MessageBox.Show(msg, "提示信息", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+        public FMTest()
+        {
+            InitializeComponent();
+        }
+
+        private void FMTest_Load(object sender, EventArgs e)
+        {
+           // MessageBox
+        }
+    }
+}
